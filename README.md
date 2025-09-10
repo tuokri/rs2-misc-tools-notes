4b594025a69cd3a86140235e99d0c261f5f7f794
Tuomo Kriikkula <tuokri@tuta.io>
2025-09-10 19:58:36 +0300

    Project init and 2025 September server notes/snippets

---
+++ README.md
@@ -0,0 +1,1 @@
+Storing various code snippets and notes related to RS2 modding work so I don't forget.
---
+++ server_2025_september/log_custom.asm
@@ -0,0 +1,4 @@
+mov rcx,qword ptr ds:[7FF7CCD367E8]
+lea r8,qword ptr ds:[7FF7CCAB4AD0]
+mov edx,2F8
+call vngame.7FF7CB96BF80
