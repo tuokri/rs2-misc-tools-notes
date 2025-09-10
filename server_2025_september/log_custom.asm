30e991f26b22f8f447027e34e73af016b7c9aa92
Tuomo Kriikkula <tuokri@tuta.io>
2025-09-10 20:38:29 +0300

    Add September 2025 server files

---
+++ server_2025_september/log_custom.asm
@@ -0,0 +1,4 @@
+mov rcx,qword ptr ds:[7FF6182797E8]
+lea r8,qword ptr ds:[7FF617FF7A70]
+mov edx,2F8
+call vngame.7FF616EAD100
---
+++ server_2025_september/switcheroo.asm
@@ -0,0 +1,5 @@
+mov r9d,1
+nop
+mov edx,3
+nop
+jmp vngame.7FF61783E981
