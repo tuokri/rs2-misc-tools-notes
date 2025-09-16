763d4dc5b3cb7454fb8d7c71bbe08a99f7c0ff8e
Tuomo Kriikkula <tuokri@tuta.io>
2025-09-16 19:22:17 +0300

    Add switcheroo.asm comments

--- server_2025_september/switcheroo.asm
+++ server_2025_september/switcheroo.asm
@@ -1,5 +1,5 @@
-mov r9d,1
-nop
-mov edx,3
-nop
-jmp vngame.7FF61783E981
+mov r9d,1                       # Force set GPackagesPassedMD5Checks to true.
+nop                             # Padding.
+mov edx,3                       # Force set GOfficialGameServerStatus to 3 (PASSED?).
+nop                             # Padding.
+jmp vngame.7FF61783E981         # Skip the rest of the checks.
